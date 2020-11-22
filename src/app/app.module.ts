67c63cf3ac473cb28aedfc47f719db7d18f48858
Elham Bolury <eli@elhams-mbp.attlocal.net>
2020-11-21 19:06:39 -0500

    added the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SalesComponent } from './sales/sales.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalesComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
